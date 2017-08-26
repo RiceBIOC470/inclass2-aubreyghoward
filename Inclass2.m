569b73d8d945f76e15524a030039a0ea8b365081
aubreyghoward <aubrey.g.howard@rice.edu>
2017-08-26 15:00:30 -0500

    Question 2/3 Answer

--- Inclass2.m
+++ Inclass2.m
@@ -3,6 +3,7 @@
 % 1. a. fill in this loop with a conditional statement so that it displays
 % only the odd numbers
 for ii = 1:20
+    % Adam Howard answer:
     d = (ii-1)/2;
     x = floor(d);
     if d == x
@@ -11,6 +12,8 @@ for ii = 1:20
 end
 
 % b. Write a new loop to display the same odd numbers which doesn't use a conditional statement 
+
+% Adam Howard answer:
 x = [1:20]';
 d = 1;
 while d < 20;
@@ -20,6 +23,8 @@ end
 
 % c. display the same odd numbers without a loop or conditional (hint use
 %  num2str)
+
+% Adam Howard answer:
 x = [1:2:20]';
 disp([num2str(x)])
 
@@ -27,28 +32,42 @@ disp([num2str(x)])
 %without using a loop). 
 
 %a. 
-
 for ii = 1:100
     z(ii) = ii*ii;
+    disp(z(ii))
 end
+% Adam Howard answer:
+x = 1:100;
+x2 = x .* x;
+x2';
 
-
-
-%%
 %b. 
 z = 0;
 for ii = 1:100
-    z = z + ii;
+    z = z + ii
 end
+
+% Adam Howard answer:
+x = 1:100;
+q = sum(x)
 
 %c.
 xx = rand(50,2);
 
 for ii = 1:50
-    z(ii) = sqrt(xx(ii,1)^2+xx(ii,x)^2);
+z(ii) = sqrt(xx(ii,1)^2+xx(ii,2)^2)
 end
 
+'end'
+% Adam Howard answer:
+
+k = sqrt ( (xx(:,1).^2) + (xx(:,2).^2))
+
+
 % d. what is the formula in c computing?
+
+%Adam Howard answer: The equation in part 2c is calculating the solution to
+%the Pythagorean Theorem for 50 random triangles. 
 
 %% plotting
 
@@ -56,9 +75,29 @@ end
 %of sin(x) vs x is a red-dashed line and the graph of cos(x) vs x is a
 %blue solid line.
 
+%Adam Howard answer:
+x = 0:0.1:(4*pi);
+
+sinx = [sin(x)]';
+cosx = [cos(x)]';
+
+plot (x,sinx,'r--',x,cosx,'b-')
+
+
 %b. repeat the above but where each graph is in a different subplot in the
 % same figure. 
 
+%Adam Howard Answer:
+x = 0:0.1:(4*pi);
+
+sinx = [sin(x)]';
+cosx = [cos(x)]';
+
+plot (x,sinx,'r--')
+hold on
+plot (x,cosx,'b-')
+
+%%
 %c. here is some data, imagine they are x and y coordinates for cells. 
 xy_dat = rand(100,2);
 %plot the data in the xy plane.
